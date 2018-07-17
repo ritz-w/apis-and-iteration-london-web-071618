492db0fd83a7adc5405cd7ddc8f577af8c0c629e
github username <github email address>
2018-07-17 16:33:02 +0100

    bonus questions

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,3 +6,9 @@ def get_character_from_user
   puts "please enter a character"
   return gets.chomp
 end
+
+
+def get_info_from_user
+  puts "please enter what you would like to know - select from "films", "appearance" or "homeworld"."
+  return gets.chomp
+end
